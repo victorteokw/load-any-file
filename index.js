9f610d43bb829b6c0d9806a9269f0a9ef0721843
Zhang Kaiyu <yeannylam@gmail.com>
2019-04-22 20:36:57 +0800

    load file from relative path

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const loadIt = () => {
+const path = require('path');
+const fs = require('fs');
+const matchExt = require('./matchExt');
+const loaders = require('./lib/loaders');
 
+const loadIt = (location) => {
+  const requirerDirectory =
+    module.parent ? path.dirname(module.parent.filename) : process.cwd();
+  let finalPath = path.resolve(requirerDirectory, location);
+  if (path.extname(finalPath) === '') {
+    finalPath = matchExt(finalPath);
+  }
+  if (!fs.existsSync(finalPath)) {
+    throw new Error(`Cannot find file '${finalPath}'.`);
+  }
+  const ext = path.extname(finalPath).substr(1);
+  const loader = loaders[ext];
+  if (!loader) {
+    throw new Error(`Unsupported file type '.${ext}'`);
+  }
+  return loader(finalPath);
 };
 
 module.exports = loadIt;
