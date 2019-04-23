b8d44bf35ea519b38ae5d916ce8bcb436f53b83f
Zhang Kaiyu <yeannylam@gmail.com>
2019-04-23 10:17:05 +0800

    dependency checking tests

---
+++ tests/dependency-error/config.ts
@@ -0,0 +1,3 @@
+export = {
+  val: 'ue'
+}
--- tests/dependency-error/test.js
+++ tests/dependency-error/test.js
@@ -3,23 +3,84 @@ const fs = require('fs');
 const assert = require('assert');
 const loadIt = require('../../index');
 
-describe('Throws error: ', function() {
-  let original;
-  beforeEach(function() {
-    original = path.join(__dirname, '../../node_modules/coffeescript');
+const alterDependency = (name) => {
+  return () => {
+    const original = path.join(__dirname, '../../node_modules/' + name);
     fs.renameSync(original, original + '_alt');
-  });
-  afterEach(function() {
-    fs.renameSync(original + '_alt', original);
-  });
-  it('if requires cson file and coffeescript is not installed', function() {
-    assert.throws(
-      () => loadIt(path.join(__dirname, './config.cson')),
-      {
-        name: 'LoadItError',
-        message: "Dependency missing. Please install 'coffeescript'."
-      }
-    );
-  });
+  };
+};
 
+const recoverDependency = (name) => {
+  return () => {
+    const original = path.join(__dirname, '../../node_modules/' + name);
+    fs.renameSync(original + '_alt', original);
+  };
+};
+
+describe('Throws dependency missing error: ', function() {
+  describe('when requiring ts file', function() {
+    beforeEach(alterDependency('typescript'));
+    afterEach(recoverDependency('typescript'));
+    it('typescript is not installed but ts-node is installed', function() {
+      assert.throws(
+        () => loadIt(path.join(__dirname, './config.ts')),
+        {
+          name: 'LoadItError',
+          message: "Dependency missing. Please install 'typescript'."
+        }
+      );
+    });
+  });
+  describe('when requiring ts file', function() {
+    beforeEach(alterDependency('ts-node'));
+    afterEach(recoverDependency('ts-node'));
+    it('ts-node is not installed but typescript is installed', function() {
+      assert.throws(
+        () => loadIt(path.join(__dirname, './config2.ts')),
+        {
+          name: 'LoadItError',
+          message: "Dependency missing. Please install 'ts-node'."
+        }
+      );
+    });
+  });
+  describe('when requiring cson file', function() {
+    beforeEach(alterDependency('coffeescript'));
+    afterEach(recoverDependency('coffeescript'));
+    it('coffeescript is not installed', function() {
+      assert.throws(
+        () => loadIt(path.join(__dirname, './config.cson')),
+        {
+          name: 'LoadItError',
+          message: "Dependency missing. Please install 'coffeescript'."
+        }
+      );
+    });
+  });
+  describe('when requiring coffee file', function() {
+    beforeEach(alterDependency('coffeescript'));
+    afterEach(recoverDependency('coffeescript'));
+    it('coffeescript is not installed', function() {
+      assert.throws(
+        () => loadIt(path.join(__dirname, './config.coffee')),
+        {
+          name: 'LoadItError',
+          message: "Dependency missing. Please install 'coffeescript'."
+        }
+      );
+    });
+  });
+  describe('when requiring yaml file', function() {
+    beforeEach(alterDependency('js-yaml'));
+    afterEach(recoverDependency('js-yaml'));
+    it('js-yaml is not installed', function() {
+      assert.throws(
+        () => loadIt(path.join(__dirname, './config.yaml')),
+        {
+          name: 'LoadItError',
+          message: "Dependency missing. Please install 'js-yaml'."
+        }
+      );
+    });
+  });
 });
