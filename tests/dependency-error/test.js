f5f71551588f99f96d5dcb6e1a6a6189a0e15c42
Zhang Kaiyu <yeannylam@gmail.com>
2019-04-23 09:59:41 +0800

    better way to test dependency missing

--- tests/dependency-error/test.js
+++ tests/dependency-error/test.js
@@ -1,63 +1,25 @@
-// const path = require('path');
-// const assert = require('assert');
-// const loadIt = require('../../index');
-//
-// describe('Throws error: ', function() {
-//   const savedRequire = require;
-//   beforeEach(function() {
-//     global.require = function(filename) {
-//       switch (filename) {
-//         case 'coffeescript/register': {
-//           const err = new Error(
-//             "Dependency missing. Please install 'coffeescript'."
-//           );
-//           err.code = 'MODULE_NOT_FOUND';
-//           throw err;
-//         }
-//         case 'coffeescript': {
-//           const err = new Error(
-//             "Dependency missing. Please install 'coffeescript'."
-//           );
-//           err.code = 'MODULE_NOT_FOUND';
-//           throw err;
-//         }
-//         case 'typescript': {
-//           const err = new Error(
-//             "Dependency missing. Please install 'typescript'."
-//           );
-//           err.code = 'MODULE_NOT_FOUND';
-//           throw err;
-//         }
-//         case 'ts-node/register': {
-//           const err = new Error(
-//             "Dependency missing. Please install 'ts-node'."
-//           );
-//           err.code = 'MODULE_NOT_FOUND';
-//           throw err;
-//         }
-//         case 'js-yaml': {
-//           const err = new Error(
-//             "Dependency missing. Please install 'js-yaml'."
-//           );
-//           err.code = 'MODULE_NOT_FOUND';
-//           throw err;
-//         }
-//         default:
-//           return savedRequire(filename);
-//       }
-//     };
-//   });
-//   afterEach(function() {
-//     global.require = savedRequire;
-//   });
-//   it('if requires cson file and coffeescript is not installed', function() {
-//     assert.throws(
-//       () => loadIt(path.join(__dirname, './config.cson')),
-//       {
-//         name: 'LoadItError',
-//         message: "Dependency missing. Please install 'coffeescript'."
-//       }
-//     );
-//   });
-//
-// });
+const path = require('path');
+const fs = require('fs');
+const assert = require('assert');
+const loadIt = require('../../index');
+
+describe('Throws error: ', function() {
+  let original;
+  beforeEach(function() {
+    original = path.join(__dirname, '../../node_modules/coffeescript');
+    fs.renameSync(original, original + '_alt');
+  });
+  afterEach(function() {
+    fs.renameSync(original + '_alt', original);
+  });
+  it('if requires cson file and coffeescript is not installed', function() {
+    assert.throws(
+      () => loadIt(path.join(__dirname, './config.cson')),
+      {
+        name: 'LoadItError',
+        message: "Dependency missing. Please install 'coffeescript'."
+      }
+    );
+  });
+
+});
